a0528b7fb0aa690f8edb47e5eeaeb8fc35270bf6
Uwe Schmidt <uwe@home>
2014-02-27 14:36:22 +0100

    2-dimensional search completed
    using this search use
    import qualified Data.StringMap.Dim2Search as Dim2
    ...
    ... Dim2.lookupLE ... Dim2.lookupGE ...
    ... Dim2.lookupRange ...

--- Data/StringMap/Dim2Search.hs
+++ Data/StringMap/Dim2Search.hs
@@ -1,14 +1,13 @@
-{-# LANGUAGE BangPatterns       #-}
 {-# LANGUAGE CPP                #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 
 module Data.StringMap.Dim2Search
-{-
+-- {-
     ( lookupGE
     , lookupLE
     , lookupRange
     )
--}
+-- -}
 where
 
 import           Data.StringMap.Base hiding (lookupGE, lookupLE, lookupRange)
@@ -16,18 +15,23 @@ import           Data.StringMap.Base hiding (lookupGE, lookupLE, lookupRange)
 -- ----------------------------------------
 
 -- | remove all entries from the map with key less than the argument key
--- 2. try
+
+lookupGE                        :: Key -> StringMap a -> StringMap a
+lookupGE                        = lookupGEX
 
 -- start comparing first dimension
 
 lookupGEX                       :: Key -> StringMap a -> StringMap a
 lookupGEX k0                    = look k0 . norm
     where
+    look [] (Val v' _t')        = (Val v' empty)
     look [] t                   = t
     look k@(c : k1) (Branch c' s' n')
-        | c <  c'               = branch c' (lookupGE2 k1 s') $ lookupGEX k n'
-        | c == c'               = branch c' (lookupGEY k1 s') n'
-        | otherwise             = lookupGEX k n'
+        | c <  c'               = branch c' (lookupGE2 k1 s') rest
+        | c == c'               = branch c' (lookupGEY k1 s') rest
+        | otherwise             =                             rest
+        where
+          rest                  = lookupGEX k n'
     look _          Empty       = empty
     look k         (Val _v' t') = lookupGEX k t'
     look _ _                    = normError "lookupGEX"
@@ -37,11 +41,14 @@ lookupGEX k0                    = look k0 . norm
 lookupGEY                       :: Key -> StringMap a -> StringMap a
 lookupGEY k0                    = look k0 . norm
     where
+    look [] (Val v' _t')        = (Val v' empty)
     look [] t                   = t
     look k@(c : k1) (Branch c' s' n')
-        | c <  c'               = branch c' (lookupGE2 k1 s') $ lookupGEY k n'
-        | c == c'               = branch c' (lookupGEX k1 s') n'
-        | otherwise             = lookupGEY k n'
+        | c <  c'               = branch c' (lookupGE2 k1 s') rest
+        | c == c'               = branch c' (lookupGEX k1 s') rest
+        | otherwise             =                             rest
+        where
+          rest                  = lookupGEY k n'
     look _          Empty       = empty
     look k         (Val _v' t') = lookupGEY k t'
     look _ _                    = normError "lookupGEY"
@@ -51,6 +58,7 @@ lookupGEY k0                    = look k0 . norm
 lookupGE2                      :: Key -> StringMap a -> StringMap a
 lookupGE2 k0                   = look k0 . norm
     where
+    look [] (Val v' _t')        = (Val v' empty)
     look [] t                   = t
     look k@(c : k1) t@(Branch c' s' n')
         | c <  c'               = t
@@ -62,9 +70,10 @@ lookupGE2 k0                   = look k0 . norm
 
 -- skip char _c and continue with comparison of next char
 
-lookupGE1                 	:: Key -> StringMap a -> StringMap a
+lookupGE1                       :: Key -> StringMap a -> StringMap a
 lookupGE1 k0               = look k0 . norm
     where
+    look [] (Val v' _t')        = (Val v' empty)
     look [] t                   = t
     look k@(_c : k1) (Branch c' s' n')
                                 = branch c' (lookupGE2 k1 s') $ lookupGE1 k n'
@@ -72,85 +81,80 @@ lookupGE1 k0               = look k0 . norm
     look k         (Val _v' t') = lookupGE1 k t'
     look _ _                    = normError "lookupGE1"
 
-
-
--- | remove all entries from the map with key less than the argument key
--- 1. try
-
-lookupGE                        :: Key -> StringMap a -> StringMap a
-lookupGE k0                     = look k0 . norm
-    where
-    look [] t                   = t
-    look k@(x : k1) (Branch x' s' n')
-        | x <  x'               = branch x' (lsX k1 . norm $ s') n'
-        | x == x'               = branch x' (eqX k1 . norm $ s') n' -- branch x' (lookupGE k1 s') n'
-        | otherwise             =            look k . norm $ n'	    -- lookupGE k n'
-    look _          Empty       = empty
-    look k         (Val _v' t') = look k . norm $ t'		    -- lookupGE k t'
-    look _ _                    = normError "lookupGE"
-
-    eqX [] t                    = t
-    eqX k@(y : k1) (Branch y' s' n')
-        | y <  y'               = branch y' (leX  k1 . norm $ s') n'
-        | y == y'		= branch y' (look k1 . norm $ s') n'
-        | otherwise		=            eqX  k  . norm $ n'
-    eqX _          Empty        = empty
-    eqX k          (Val _v' t') = eqX k . norm $ t'
-    eqX _ _                     = normError "eqX"
-
-    lsX [] t			= t
-    lsX k@(y : k1) t@(Branch y' s' n')
-        | y <  y'		= t
-        | y == y'		= branch y' (leY k1 . norm $ s') n'
-        | otherwise		=            lsX k  . norm $ n'
-    lsX _          Empty	= empty
-    lsX k         (Val _v' t')  = lsX k . norm $ t'
-    lsX _ _			= normError "lsX"
-
-    -- compare every second char in the sequence of key chars
-    -- to be equal to or greater than the char in the search key
-    -- starting with the first char
-    leX [] t			= t
-    leX (x : k1) t@(Branch x' s' n')
-        | x <  x'		= t
-        | x == x'		= branch x' (leY k1 . norm $ s') n'
-        | otherwise		= empty
-    leX _          Empty 	= empty
-    leX k          (Val _v' t') = leX k . norm $ t'	-- or val _v' (leX ...) ???
-    leX _ _                     = normError "leX"
-
-    -- like leX, but starting with the second key char
-    leY [] t			= t
-    leY k@(_y : k1) (Branch y' s' n')
-				= branch y' (leX k1 . norm $ s') $ (leY k . norm $ n')
-    leY _          Empty        = empty
-    leY k          (Val _v' t') = leY k . norm $ t'	-- or val _v' (leY ...) ???
-    leY _ _			= normError "leY"
-
--- | remove all entries from the map with keys not having the argument key
--- as prefix and are larger than the argument key
+-- ----------------------------------------
+--
+-- the same stuff for less or equal
 
 lookupLE                        :: Key -> StringMap a -> StringMap a
-lookupLE k0                     = look k0 . norm
+lookupLE                        = lookupLEX
+
+-- start comparing first dimension
+
+lookupLEX                       :: Key -> StringMap a -> StringMap a
+lookupLEX k0                    = look k0 . norm
     where
     look [] (Val v' _t')        = (Val v' empty)
     look [] _t                  = empty
     look k@(c : k1) (Branch c' s' n')
-        | c <  c'               = empty
-        | c == c'               = branch c' (lookupLE k1 s') empty
-        | otherwise             = branch c' s' (lookupLE k n')
+        | c <  c'               =                             empty
+        | c == c'               = branch c' (lookupLEY k1 s') empty
+        | otherwise             = branch c' (lookupLE2 k1 s') (lookupLEX k n')
     look _          Empty       = empty
-    look k         (Val v' t')  = val v' (lookupLE k t')
-    look _ _                    = normError "lookupLE"
+    look k         (Val v' t')  = val v' (lookupLEX k t')
+    look _ _                    = normError "lookupLEX"
+
+-- continue comparing second dimension
+
+lookupLEY                       :: Key -> StringMap a -> StringMap a
+lookupLEY k0                    = look k0 . norm
+    where
+    look [] (Val v' _t')        = (Val v' empty)
+    look [] _t                  = empty
+    look k@(c : k1) (Branch c' s' n')
+        | c <  c'               =                             empty
+        | c == c'               = branch c' (lookupLEX k1 s') empty
+        | otherwise             = branch c' (lookupLE2 k1 s') (lookupLEY k n')
+    look _          Empty       = empty
+    look k         (Val v' t')  = val v' (lookupLEY k t')
+    look _ _                    = normError "lookupLEY"
+
+-- ordering of one dim is o.k., check only 2. dim (every second char)
+
+lookupLE2                      :: Key -> StringMap a -> StringMap a
+lookupLE2 k0                   = look k0 . norm
+    where
+    look [] (Val v' _t')        = (Val v' empty)
+    look [] _t                  = empty
+    look k@(c : k1) (Branch c' s' n')
+        | c <  c'               =                             empty
+        | c == c'               = branch c' (lookupLE1 k1 s') empty
+        | otherwise             = branch c' s'                (lookupLE2 k n')
+    look _          Empty       = empty
+    look k         (Val v' t')  = val v' (lookupLE2 k t')
+    look _ _                    = normError "lookupLE2"
+
+-- skip char _c and continue with comparison of next char
+
+lookupLE1                       :: Key -> StringMap a -> StringMap a
+lookupLE1 k0                    = look k0 . norm
+    where
+    look [] (Val v' _t')        = (Val v' empty)
+    look [] t                   = t
+    look k@(_c : k1) (Branch c' s' n')
+                                = branch c' (lookupLE2 k1 s') (lookupLE1 k n')
+    look _          Empty       = empty
+    look k         (Val v' t')  = val v' (lookupLE1 k t')
+    look _ _                    = normError "lookupLE1"
+
 
 -- | Combination of 'lookupLE' and 'lookupGE'
--- 
+--
 -- > keys $ lookupRange "a" "b" $ fromList $ zip ["", "a", "ab", "b", "ba", "c"] [1..] = ["a","ab","b"]
--- 
+--
 -- For all keys in @k = keys $ lookupRange lb ub m@, this property holts true: @k >= ub && k <= lb@
 
 lookupRange                     :: Key -> Key -> StringMap a -> StringMap a
-lookupRange lb ub               = lookupLE ub . lookupGE lb
+lookupRange lb ub               = lookupGE lb . lookupLE ub
 
 -- ----------------------------------------
 
--- tests/Dim2Test.hs
+++ tests/Dim2Test.hs
@@ -1,8 +1,15 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+
 module Main where
 
-import qualified Data.StringMap as M
+import           Data.List                 (nub, sort)
+import qualified Data.StringMap            as M
 import qualified Data.StringMap.Dim2Search as D2
 
+-- ----------------------------------------
+--
+-- auxiliary functions for mapping pairs of Ints to Strings and vice versa
 
 intToKey :: Int -> Int -> Int -> String
 intToKey base len val = tok len val ""
@@ -32,22 +39,14 @@ unMerge (x : y : s) = (x : xs, y : ys)
     where
       (xs, ys) = unMerge s
 
-
--- the real point
-
-type Point = (Int, Int)
-
-le :: Point -> Point -> Bool
-le (x1, y1) (x2, y2)
-    = x1 <= x2 && y1 <= y2
-
--- the point as string
-
-newtype Point' = P' String
-    deriving (Eq)
-
-instance Show Point' where
-    show (P' ds) = ds
+-- ----------------------------------------
+--
+-- experiment to understand 2-dimensional location
+-- search implemented by using the StringMap impl.
+--
+-- an ordering on strings (representing pairs of ints)
+-- that is isomorphic to the partial ordering
+-- used for 2-dimensional search
 
 instance Ord Point' where
     (P' s1) <= (P' s2) = s1 `le` s2
@@ -75,13 +74,13 @@ instance Ord Point' where
 -- toPoint' and fromPoint': the bijection Point <-> Point'
 
 toPoint' :: Point -> Point'
-toPoint' p = P' $ intPairToKey base len p
+toPoint' (P p) = P' $ intPairToKey base len p
     where
       base =  2		-- or 10
       len  =  10        -- or  3  (or something else)
 
 fromPoint' :: Point' -> Point
-fromPoint' (P' ds) = (intFromKey xs, intFromKey ys)
+fromPoint' (P' ds) = P (intFromKey xs, intFromKey ys)
     where
       (xs, ys) = unMerge ds
 
@@ -96,28 +95,172 @@ propTest n
     = filter (not . uncurry propOrdered) qs
       where
         xs = [1..n]
-        ps = [(x, y) | x <- xs, y <- xs]
+        ps = [P (x, y) | x <- xs, y <- xs]
         qs = [(p1, p2) | p1 <- ps, p2 <- ps]
 
-test :: Bool
-test = null $ propTest 20
+test1 :: Bool
+test1 = null $ propTest 20
 
-main :: IO ()
-main
-    = do print (propTest 20)
-         return ()
+-- ----------------------------------------
 
-type D2Map = M.StringMap ()
+newtype Point     = P   {unP :: (Int, Int)    } deriving (Eq)
+newtype PointSet  = PS  {unPS :: [Point]       } deriving (Eq)
+                                        	-- assuming only smart constructor mkPS is used
 
-mkD2 :: [Int] -> D2Map
-mkD2 = M.fromList . map (\x -> (intPairToKey 10 5 (x, x), ()))
+newtype Point'    = P'  {unP' :: String        } deriving (Eq)
+newtype PointSet' = PS' {unPS' :: M.StringMap ()} deriving (Eq)
 
-unD2 :: D2Map -> [(Int, Int)]
-unD2 = map (\x -> let (xs, ys) = unMerge x in (intFromKey xs, intFromKey ys)) . M.keys
+instance Show Point     where show = show . unP
+instance Show Point'    where show = show . unP'
+instance Show PointSet  where show = show . unPS
+instance Show PointSet' where show = show . M.keys . unPS'
 
-d1 :: D2Map
+class PartOrd a where
+    le :: a -> a -> Bool
+    ge :: a -> a -> Bool
+
+instance PartOrd Point where
+    (P (x1, y1)) `le` (P (x2, y2))
+        = x1 <= x2 && y1 <= y2
+
+    (P (x1, y1)) `ge` (P (x2, y2))
+        = x1 >= x2 && y1 >= y2
+
+instance PartOrd Point' where
+    (P' p1) `le` (P' p2)
+        = not . M.null . D2.lookupLE p2 $ (M.singleton p1 ())
+
+    (P' p1) `ge` (P' p2)
+        = not . M.null . D2.lookupGE p2 $ (M.singleton p1 ())
+
+class Lookup p s | s -> p where
+    lookupLE :: p -> s -> s
+    lookupGE :: p -> s -> s
+
+instance Lookup Point PointSet where
+    lookupLE p ps = PS . filter (`le` p) . unPS $ ps
+    lookupGE p ps = PS . filter (`ge` p) . unPS $ ps
+
+instance Lookup Point' PointSet' where
+    lookupLE p ps = PS' . D2.lookupLE (unP' p) . unPS' $ ps
+    lookupGE p ps = PS' . D2.lookupGE (unP' p) . unPS' $ ps
+
+-- the bijection between Point and Point'
+
+pToP' :: Point -> Point'
+pToP' = P' . intPairToKey 10 5 . unP	-- base 10, 5 digits
+
+p'ToP :: Point' -> Point
+p'ToP (P' p') = P (intFromKey xs, intFromKey ys)
+    where
+      (xs, ys) = unMerge p'
+
+-- the bijection between PointSet and PointSet'
+
+psToPS' :: PointSet -> PointSet'
+psToPS' = PS' . M.fromList . map (\(P' x) -> (x, ())) . map pToP' . unPS
+
+ps'ToPS :: PointSet' -> PointSet
+ps'ToPS = mkPS . map (unP . p'ToP . P') . M.keys . unPS'
+
+mkP :: Int -> Int -> Point
+mkP x y = P (x, y)
+
+mkP' :: Int -> Int -> Point'
+mkP' x y = pToP' $ mkP x y
+
+mkPS :: [(Int, Int)] -> PointSet
+mkPS = PS . map P . sort
+
+mkPS' :: [(Int, Int)] -> PointSet'
+mkPS' = psToPS' . mkPS
+
+mkxx :: Int -> Point
+mkxx i = mkP i i
+
+mkxx' :: Int -> Point'
+mkxx' = pToP' . mkxx
+
+mkD2 :: [Int] -> PointSet
+mkD2 = PS . map mkxx
+
+mkD2' :: [Int] -> PointSet'
+mkD2' = psToPS' . mkD2
+
+d1 :: PointSet
 d1 = mkD2 [1,10,100,105,107,125,200, 205, 222]
 
-d0 :: D2Map
-d0 = mkD2 [10]
+d1' :: PointSet'
+d1' = psToPS' d1
 
+d2 :: PointSet
+d2 = mkD2 [2,10,20,25,100,111,155,200,333,500]
+
+d2' :: PointSet'
+d2' = psToPS' d2
+
+d0' :: PointSet'
+d0' = mkD2' [10,100]
+
+
+mkSquare :: Int -> Int -> PointSet
+mkSquare n m = mkPS [(i, j) | i <- [n..m], j <- [n..m]]
+
+-- input list must contain at least 3 different elements
+mkPointPointSet :: [Int] -> ([Point], PointSet)
+mkPointPointSet xs0
+    = (ps, ps')
+      where
+        xs@(_ : ys@(_:_:_)) = sort xs0
+        xs'                  = init ys
+        ps  =      [mkP i j | i <- xs,  j <- xs ]
+        ps' = mkPS [ (i, j) | i <- xs', j <- xs']
+
+
+ps1 :: PointSet
+xs1 :: [Point]
+(xs1, ps1) =  mkPointPointSet [1,2,10,20,25,100,111,155,200,333,500,505]
+
+lawBijection :: PointSet -> Bool
+lawBijection ps
+    = ps == (ps'ToPS . psToPS' $ ps)
+
+lawPredicateMorphism :: (Point -> Bool) -> (Point' -> Bool) ->
+                        Point -> Bool
+lawPredicateMorphism p p' x
+    = p x == (p' $ pToP' x)
+
+lawPredicate2Morphism :: (Point -> Point -> Bool) -> (Point' -> Point' -> Bool) ->
+                         Point -> Point -> Bool
+lawPredicate2Morphism p2 p2' x y
+    = lawPredicateMorphism (p2 x) (p2' $ pToP' x) y
+
+lawPointSetMorphism :: (PointSet -> PointSet) -> (PointSet' -> PointSet') ->
+                       PointSet -> Bool
+lawPointSetMorphism f f' ps
+    = f ps == (ps'ToPS . f' . psToPS' $ ps)
+
+lawLookupGE :: Point -> PointSet -> Bool
+lawLookupGE p ps = lawPointSetMorphism (lookupGE p) (lookupGE $ pToP' p) ps
+
+lawLookupLE :: Point -> PointSet -> Bool
+lawLookupLE p ps = lawPointSetMorphism (lookupLE p) (lookupLE $ pToP' p) ps
+
+testPointPointSet :: (Point -> PointSet -> Bool) -> ([Point], PointSet) -> [Point]
+testPointPointSet law (xs, ps)
+    = filter (\p -> not $ law p ps) xs
+
+testLookup :: ([Point], PointSet) -> Bool
+testLookup ps
+    = null (testPointPointSet lawLookupLE ps)
+      &&
+      null (testPointPointSet lawLookupGE ps)
+
+theTest :: Bool
+theTest = testLookup $
+       mkPointPointSet [1,2,10,20,25,100,111,155,200,333,500,505]
+
+main :: IO ()
+main = print theTest >> return ()
+
+-- ----------------------------------------
